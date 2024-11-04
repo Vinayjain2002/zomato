c594aee5a68556f75a7a477f6417c8b7bb83b336
Vinay Jain <vinayjain61787@gmail.com>
2024-11-04 10:41:55 +0530

    Updated

--- Schemas/Query.js
+++ Schemas/Query.js
@@ -1,12 +1,36 @@
 const qraphql = require('graphql')
-// define al the queries import statement
-
 const { GraphQLObjectType } = graphql
+import { AllDeliveryPartner,SearchDeliveryPartner,particularDeliveryPartner} from './queries/DeliveryPartnerQuery.js'
+import { findAllItems,particularItem,searchItem} from './queries/MenuQuery.js'
+import { findRestaurantByOwner,searchRestaurant, particularRestaurant, fetchAllRestaurants} from './queries/RestaurantQuery.js'
+import {restaurantReviews,userReviews,particularOrderReview} from './queries/ReviewQuery.js'
+import {  login, getUserDetail,getAllUsers, getAllDeliveryPartner,getAllRestaurnatOwner,getAllAdmin} from './queries/UserQuery.js'
 
 const Query= new GraphQLObjectType({
     name: "Query",
     fields:{
-
+        AllDeliveryPartner,
+        SearchDeliveryPartner,
+        particularDeliveryPartner,
+        findAllItems,
+        particularItem,
+        searchItem,
+        fetchUserAllOrders,
+        fetchOrderwithRestaurant,
+        searchOrder,
+        particularOrder,
+        findRestaurantByOwner,
+        searchRestaurant,
+        particularRestaurant,
+        fetchAllRestaurants,
+        restaurantReviews,
+    userReviews,
+    particularOrderReview,  login,
+    getUserDetail,
+    getAllUsers,
+    getAllDeliveryPartner,
+    getAllRestaurnatOwner,
+    getAllAdmin
     }
 });
 
